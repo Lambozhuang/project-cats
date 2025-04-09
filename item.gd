545c6f21c47d918f3f793704044c9c6b1f8d2f45
Lambozhuang <Lambozhuang@gmail.com>
2025-04-10 00:16:35 +0200

    Cleanup and replace item sprite

--- item.gd
+++ item.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Node2D
 
 var isCarried = false
 
@@ -9,7 +9,9 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if isCarried == true:
-		self.position = get_node("../Player/Marker2D").get_global_position()
+		var attach_point = get_node("../../Player/Marker2D").get_global_position()
+		var offset = Vector2(40, 30)  # Adjust the x value as desired
+		self.position = attach_point + offset
 
 
 func _input(event: InputEvent) -> void:
@@ -17,15 +19,15 @@ func _input(event: InputEvent) -> void:
 		print("carry the item")
 		var bodies = $Area2D.get_overlapping_bodies()
 		for body in bodies:
-			if body.name == "Player" and get_node("../Player").canCarry == true:
+			if body.name == "Player" and get_node("../../Player").canCarry == true:
 				isCarried = true
-				get_node("../Player").canCarry = false
+				get_node("../../Player").canCarry = false
 				
 	elif Input.is_action_just_released("carry"):
 		if isCarried:
 			print("release the item")
 			isCarried = false
-			get_node("../Player").canCarry = true
+			get_node("../../Player").canCarry = true
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("touch item")
